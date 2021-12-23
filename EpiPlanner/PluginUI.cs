5a5d2527db5b8fc188041f85d468c1e9c20dcf3a
Christopher Jennison <cjennison92@gmail.com>
2021-12-23 10:14:20 -0500

    add epiPlanner dir

---
+++ EpiPlanner/PluginUI.cs
@@ -0,0 +1,111 @@
+﻿using ImGuiNET;
+using Dalamud.Logging;
+using System;
+using System.Numerics;
+
+namespace EpiPlan
+{
+  // It is good to have this be disposable in general, in case you ever need it
+  // to do any cleanup
+  class PluginUI : IDisposable
+  {
+    private Configuration configuration;
+
+    private ImGuiScene.TextureWrap goatImage;
+
+    // this extra bool exists for ImGui, since you can't ref a property
+    private bool visible = false;
+    public bool Visible
+    {
+      get { return this.visible; }
+      set { this.visible = value; }
+    }
+
+    private bool settingsVisible = false;
+    public bool SettingsVisible
+    {
+      get { return this.settingsVisible; }
+      set { this.settingsVisible = value; }
+    }
+
+    // passing in the image here just for simplicity
+    public PluginUI(Configuration configuration, ImGuiScene.TextureWrap goatImage)
+    {
+      this.configuration = configuration;
+      this.goatImage = goatImage;
+    }
+
+    public void Dispose()
+    {
+      this.goatImage.Dispose();
+    }
+
+    public void Draw()
+    {
+      // This is our only draw handler attached to UIBuilder, so it needs to be
+      // able to draw any windows we might have open.
+      // Each method checks its own visibility/state to ensure it only draws when
+      // it actually makes sense.
+      // There are other ways to do this, but it is generally best to keep the number of
+      // draw delegates as low as possible.
+
+      DrawMainWindow();
+      DrawSettingsWindow();
+
+      PluginLog.Debug("Hello World");
+    }
+
+    public void DrawMainWindow()
+    {
+      if (!Visible)
+      {
+        return;
+      }
+
+      ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+      ImGui.SetNextWindowSizeConstraints(new Vector2(375, 330), new Vector2(float.MaxValue, float.MaxValue));
+      if (ImGui.Begin("EpiPlan", ref this.visible, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+      {
+        ImGui.Text($"Epi Plan is {(this.configuration.EnablePlanner ? "Enabled" : "Disabled")}");
+
+        if (ImGui.Button("Settings"))
+        {
+          SettingsVisible = true;
+        }
+
+        ImGui.Spacing();
+
+        ImGui.Text("Have a goat:");
+        ImGui.Indent(55);
+        ImGui.Image(this.goatImage.ImGuiHandle, new Vector2(this.goatImage.Width, this.goatImage.Height));
+        ImGui.Unindent(55);
+      }
+      ImGui.End();
+    }
+
+    public void DrawSettingsWindow()
+    {
+      if (!SettingsVisible)
+      {
+        return;
+      }
+
+      ImGui.SetNextWindowSize(new Vector2(232, 75), ImGuiCond.Always);
+      if (ImGui.Begin("EpiPlan Settings", ref this.settingsVisible,
+          ImGuiWindowFlags.NoResize
+              | ImGuiWindowFlags.NoCollapse
+              | ImGuiWindowFlags.NoScrollbar
+              | ImGuiWindowFlags.NoScrollWithMouse
+      )) {
+        // can't ref a property, so use a local copy
+        var configValue = this.configuration.EnablePlanner;
+        if (ImGui.Checkbox("Enable Planner", ref configValue)) {
+          this.configuration.EnablePlanner = configValue;
+          // can save immediately on change, if you don't want to provide a "Save and Close" button
+          this.configuration.Save();
+        }
+      }
+      ImGui.End();
+    }
+  }
+}
---
+++ EpiPlanner/README.md
@@ -0,0 +1,49 @@
+# EpiPlan
+Rotation planner using available logging information to help assist in the
+performance of DPS classes in FFXIV.
+
+
+## WIP
+
+
+
+
+### Whitepage
+
+EpiPlan is a rotation helper that generalizing FFXIV DPS rotation into:
+- Phases: Parts of a rotation with differing priorities. 
+- Priorities
+- Chains
+
+Inspiration is taken from World of Warcraft's Hekili addon.
+
+Each job implements its own set of these controls.
+At a basic version, EpiPlan can detect what phase a player enters into and offers guidance on the next set of
+button presses to execute.
+As this becomes more intelligent button presses can change mid-phase depending on cooldowns, buff times, and
+many other class states.
+
+EpiPlan is made up of the following critical components:
+- ActionPlan: An object that represents the set of elements that need to be activated
+- Listener: FFXIV Event hook
+- JobRunner: (per Job) computes the current action plan
+- DisplayEngine: Receives instructions to display icons via an action plan 
+- Orchestrator: Connects all major components
+
+The *ActionPlan* is basically an array of elements, or proposed actions, that should be activated in
+a specific order. The basic component, an <Action>, provides information about the name, type, cast time, mana cost,
+and other information that the display engine can use to display the Action accordingly.
+
+The *Listener* hooks into the FFXIV event system to get details about the game, echoing back cooldowns,
+action activations, and any relevant class information such as cooldowns, stacks of de/buffs, etc.
+For use of mocking, the Listener in the UITester is a Simulator UI that can send events to the Orchestrator.
+The Orchestrator for each tick will consume the Listener's gamestate. (this.listener.getState())
+
+The *JobRunner* is the central processing system. The JobRunner is a mini-orchestrator that integrates with a
+<Job>Machine (ie MCHMachine, BLMMachine) to compute the ActionPlan depending on the current state.
+
+The *DisplayEngine* consumes an ActionPlan and displays it on the UI. 
+
+The *Orchestrator* connects all of the above.
+
+![diagram](https://i.imgur.com/DODxE0e.png)
